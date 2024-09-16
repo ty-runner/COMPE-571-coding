fc5873e2e9d085a2c996019158417414722f272d
ty-runner <ty.runner02@gmail.com>
2024-09-16 15:24:22 -0700

    Testing 9 runs

--- PA1/Multitask_Option1/test.sh
+++ PA1/Multitask_Option1/test.sh
@@ -5,7 +5,9 @@ gcc dynamic_tasks.c -o dynamic.o
 values=(100000000 1000000000 10000000000)
 num_tasks=(2 4 8)
 
-for i in {0..2}; do
-	echo "Running N = ${values[$i]} and Tasks = ${num_tasks[$i]}"
-	./dynamic.o "${values[$i]}" "${num_tasks[$i]}"
+for N in "${values[@]}"; do
+    for tasks in "${num_tasks[@]}"; do
+        echo "Running N = $N and Tasks = $tasks"
+        ./dynamic.o "$N" "$tasks"
+    done
 done
