4368144473603e02ad6a2971c2f5eb858f1f00fe
ty-runner <ty.runner02@gmail.com>
2024-09-16 18:34:57 -0700

    Adding in code timing

--- PA1/baseline.c
+++ PA1/baseline.c
@@ -1,17 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-unsigned long long calc_sum(unsigned long long N){
-	unsigned long long sum = 0;
-	for (unsigned long long i = 0; i < N; i++){
-		sum+=i;
-	}
-	return sum;
+unsigned long long calc_sum(unsigned long long N) {
+    unsigned long long sum = 0;
+    for (unsigned long long i = 0; i < N; i++) {
+        sum += i;
+    }
+    return sum;
 }
 
-int main (int argc, char *argv[]) {
-	unsigned long long N = strtoull(argv[1], NULL, 10);
-	unsigned long long sum = calc_sum(N);
-	printf("Sum is: %llu\n", sum);
-	return 0;
-}	
+int main(int argc, char *argv[]) {
+    unsigned long long N = strtoull(argv[1], NULL, 10);
+
+    struct timespec start_time, end_time;
+    clock_gettime(CLOCK_MONOTONIC, &start_time);
+
+    unsigned long long sum = calc_sum(N);
+
+    clock_gettime(CLOCK_MONOTONIC, &end_time);
+
+    double elapsed_time = (end_time.tv_sec - start_time.tv_sec) + (end_time.tv_nsec - start_time.tv_nsec) / 1e9;
+
+    printf("Sum is: %llu\n", sum);
+    printf("Elapsed time: %.6f seconds\n", elapsed_time);
+
+    return 0;
+}
--- PA1/test.sh
+++ PA1/test.sh
@@ -9,6 +9,6 @@ values=(100000000 1000000000 10000000000)
 for value in "${values[@]}"
 do
 	echo "Testing with N = $value"
-	time ./baseline.o $value
+	./baseline.o $value
 	echo "------------------"
 done
