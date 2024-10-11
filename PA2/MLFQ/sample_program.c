5c96752e60204388b55cdc6635530fe4954560e4
ty-runner <ty.runner02@gmail.com>
2024-10-11 15:12:02 -0700

    reset

--- PA2/MLFQ/sample_program.c
+++ PA2/MLFQ/sample_program.c
@@ -7,25 +7,29 @@
 #include <time.h> 
 #include <signal.h>
 #include <sys/time.h>
-
 /*
 ***********************************************************************************
-* These DEFINE statements represent the workload size of each task and
-* the time quantum values for each queue in MLFQ.
-***********************************************************************************/
+*************
+These DEFINE statements represent the workload size of each task and
+the time quantum values for Round Robin scheduling for each task.
+***********************************************************************************
+**************/
 #define WORKLOAD1 100000
 #define WORKLOAD2 50000
 #define WORKLOAD3 25000
 #define WORKLOAD4 10000
 
 #define QUANTUM1 5000
-#define QUANTUM2 10000
-#define QUANTUM3 15000
+#define QUANTUM2 5000
+#define QUANTUM3 5000
+#define QUANTUM4 5000
 
 /*
 ***********************************************************************************
-* DO NOT CHANGE THE FUNCTION IMPLEMENTATION
-***********************************************************************************/
+*************
+DO NOT CHANGE THE FUNCTION IMPLEMENTATION
+***********************************************************************************
+**************/
 void myfunction(int param) {
     int i = 2;
     int j, k;
@@ -50,37 +54,36 @@ int main(int argc, char const *argv[]) {
     pid_t pid1, pid2, pid3, pid4;
     int running1, running2, running3, running4;
     
-    struct timeval finish_time, start_time1, start_time2, start_time3, start_time4;
-    long response_time1 = 0, response_time2 = 0, response_time3 = 0, response_time4 = 0;
+    struct timeval finish_time, start_time1, start_time2, start_time3, start_time4; // For response time tracking
+    long response_time1, response_time2, response_time3, response_time4;
 
-    // Fork processes
     pid1 = fork();
     if (pid1 == 0) {
         myfunction(WORKLOAD1);
         exit(0);
     }
-    kill(pid1, SIGSTOP);
+    kill(pid1, SIGSTOP); // child process created but paused
 
     pid2 = fork();
     if (pid2 == 0) {
         myfunction(WORKLOAD2);
         exit(0);
     }
-    kill(pid2, SIGSTOP);
+    kill(pid2, SIGSTOP); // child process created but paused
 
     pid3 = fork();
     if (pid3 == 0) {
         myfunction(WORKLOAD3);
         exit(0);
     }
-    kill(pid3, SIGSTOP);
+    kill(pid3, SIGSTOP); // child process created but paused
 
     pid4 = fork();
     if (pid4 == 0) {
         myfunction(WORKLOAD4);
         exit(0);
     }
-    kill(pid4, SIGSTOP);
+    kill(pid4, SIGSTOP); // child process created but paused
 /**********************************************************************************
 **************
 At this point, all newly-created child processes are stopped, and
@@ -96,120 +99,80 @@ other scheduling methods
 to be implemented.
 ***********************************************************************************
 *************/
-    // Initialize response time tracking variables
-    int first_run1 = 0, first_run2 = 0, first_run3 = 0, first_run4 = 0;
     running1 = 1;
     running2 = 1;
     running3 = 1;
     running4 = 1;
-    // Queue system for MLFQ
-    int queue1[] = {pid1, pid2, pid3, pid4};
-    int queue2[4], queue3[4];
-    int q1_size = 4, q2_size = 0, q3_size = 0;
+    // Initialize response time tracking variables
+    int first_run1 = 0, first_run2 = 0, first_run3 = 0, first_run4 = 0;
 
-    while (q1_size > 0 || q2_size > 0 || q3_size > 0) {
-        // Round Robin on Queue 1
-        for (int i = 0; i < q1_size; i++) {
-            if (!first_run1 && queue1[i] == pid1) {
-                gettimeofday(&start_time1, NULL);
-                first_run1 = 1;
-            } else if (!first_run2 && queue1[i] == pid2) {
-                gettimeofday(&start_time2, NULL);
-                first_run2 = 1;
-            } else if (!first_run3 && queue1[i] == pid3) {
-                gettimeofday(&start_time3, NULL);
-                first_run3 = 1;
-            } else if (!first_run4 && queue1[i] == pid4) {
-                gettimeofday(&start_time4, NULL);
-                first_run4 = 1;
+    while (running1 > 0 || running2 > 0 || running3 > 0 || running4 > 0) {
+        if (running1 > 0) {
+            if (!first_run1) { // If this is the first run for pid1
+                gettimeofday(&start_time1, NULL); // Get start time
+                first_run1 = 1; // Mark that we have started running this process
             }
-
-            kill(queue1[i], SIGCONT);
-            usleep(QUANTUM1); // Run for QUANTUM1 time
-            kill(queue1[i], SIGSTOP);
-
-            if (waitpid(queue1[i], &running1, WNOHANG) > 0) {
-                if (queue1[i] == pid1) {
-                    gettimeofday(&finish_time, NULL);
-                    response_time1 = (finish_time.tv_sec * 1000000 + finish_time.tv_usec) - (start_time1.tv_sec * 1000000 + start_time1.tv_usec);
-                } else if (queue1[i] == pid2) {
-                    gettimeofday(&finish_time, NULL);
-                    response_time2 = (finish_time.tv_sec * 1000000 + finish_time.tv_usec) - (start_time2.tv_sec * 1000000 + start_time2.tv_usec);
-                } else if (queue1[i] == pid3) {
-                    gettimeofday(&finish_time, NULL);
-                    response_time3 = (finish_time.tv_sec * 1000000 + finish_time.tv_usec) - (start_time3.tv_sec * 1000000 + start_time3.tv_usec);
-                } else if (queue1[i] == pid4) {
-                    gettimeofday(&finish_time, NULL);
-                    response_time4 = (finish_time.tv_sec * 1000000 + finish_time.tv_usec) - (start_time4.tv_sec * 1000000 + start_time4.tv_usec);
-                }
-                // Process finished, remove from Queue 1
-                for (int j = i; j < q1_size - 1; j++) {
-                    queue1[j] = queue1[j + 1];
-                }
-                q1_size--;
-                i--;
-            } else {
-                // Move to Queue 2
-                queue2[q2_size++] = queue1[i];
-                for (int j = i; j < q1_size - 1; j++) {
-                    queue1[j] = queue1[j + 1];
-                }
-                q1_size--;
-                i--;
-            }
+            kill(pid1, SIGCONT); // Continue process 1
+            usleep(QUANTUM1); // Process 1 runs for QUANTUM1 microseconds
+            kill(pid1, SIGSTOP); // Stop process 1
         }
-
-        // Round Robin on Queue 2
-        for (int i = 0; i < q2_size; i++) {
-            kill(queue2[i], SIGCONT);
-            usleep(QUANTUM2);
-            kill(queue2[i], SIGSTOP);
-
-            if (waitpid(queue2[i], &running2, WNOHANG) > 0) {
-                if (queue2[i] == pid1 || queue2[i] == pid2 || queue2[i] == pid3 || queue2[i] == pid4) {
-                    // Process finished, no need to track more
-                }
-                for (int j = i; j < q2_size - 1; j++) {
-                    queue2[j] = queue2[j + 1];
-                }
-                q2_size--;
-                i--;
-            } else {
-                // Move to Queue 3
-                queue3[q3_size++] = queue2[i];
-                for (int j = i; j < q2_size - 1; j++) {
-                    queue2[j] = queue2[j + 1];
-                }
-                q2_size--;
-                i--;
+        if (running2 > 0) {
+            if (!first_run2) { // If this is the first run for pid2
+                gettimeofday(&start_time2, NULL); // Get start time
+                first_run2 = 1; // Mark that we have started running this process
             }
+            kill(pid2, SIGCONT); // Continue process 2
+            usleep(QUANTUM2); // Process 2 runs for QUANTUM2 microseconds
+            kill(pid2, SIGSTOP); // Stop process 2
         }
-
-        // Round Robin on Queue 3
-        for (int i = 0; i < q3_size; i++) {
-            kill(queue3[i], SIGCONT);
-            usleep(QUANTUM3);
-            kill(queue3[i], SIGSTOP);
-
-            if (waitpid(queue3[i], &running3, WNOHANG) > 0) {
-                if (queue3[i] == pid1 || queue3[i] == pid2 || queue3[i] == pid3 || queue3[i] == pid4) {
-                    // Process finished, no need to track more
-                }
-                for (int j = i; j < q3_size - 1; j++) {
-                    queue3[j] = queue3[j + 1];
-                }
-                q3_size--;
-                i--;
+        if (running3 > 0) {
+            if (!first_run3) { // If this is the first run for pid3
+                gettimeofday(&start_time3, NULL); // Get start time
+                first_run3 = 1; // Mark that we have started running this process
             }
+            kill(pid3, SIGCONT); // Continue process 3
+            usleep(QUANTUM3); // Process 3 runs for QUANTUM3 microseconds
+            kill(pid3, SIGSTOP); // Stop process 3
         }
+        if (running4 > 0) {
+            if (!first_run4) { // If this is the first run for pid4
+                gettimeofday(&start_time4, NULL); // Get start time
+                first_run4 = 1; // Mark that we have started running this process
+            }
+            kill(pid4, SIGCONT); // Continue process 4
+            usleep(QUANTUM4); // Process 4 runs for QUANTUM4 microseconds
+            kill(pid4, SIGSTOP); // Stop process 4
+        }
+        if(waitpid(pid1, &running1, WNOHANG) > 0){
+		gettimeofday(&finish_time, NULL);
+		response_time1 = (finish_time.tv_sec * 1000000 + finish_time.tv_usec) - (start_time1.tv_sec * 1000000 + start_time1.tv_usec);
+	}	
+        if(waitpid(pid2, &running2, WNOHANG) > 0){
+		gettimeofday(&finish_time, NULL);
+		response_time2 = (finish_time.tv_sec * 1000000 + finish_time.tv_usec) - (start_time2.tv_sec * 1000000 + start_time2.tv_usec);
+	}
+        if(waitpid(pid3, &running3, WNOHANG) > 0){
+		gettimeofday(&finish_time, NULL);
+		response_time3 = (finish_time.tv_sec * 1000000 + finish_time.tv_usec) - (start_time3.tv_sec * 1000000 + start_time3.tv_usec);
+	}
+        if(waitpid(pid4, &running4, WNOHANG) > 0){
+		gettimeofday(&finish_time, NULL);
+		response_time4 = (finish_time.tv_sec * 1000000 + finish_time.tv_usec) - (start_time4.tv_sec * 1000000 + start_time4.tv_usec);
+	}
     }
 
-    // Output response times
+    // Calculate response times
     printf("Response Time for Process 1: %ld microseconds\n", response_time1);
     printf("Response Time for Process 2: %ld microseconds\n", response_time2);
     printf("Response Time for Process 3: %ld microseconds\n", response_time3);
     printf("Response Time for Process 4: %ld microseconds\n", response_time4);
     printf("Average response time: %ld microseconds\n", (response_time1 + response_time2 + response_time3 + response_time4) / 4);
+    
+/**********************************************************************************
+**************
+- Scheduling code ends here
+***********************************************************************************
+*************/
 
     return 0;
 }
